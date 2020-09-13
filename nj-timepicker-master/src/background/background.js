a91965fc6f2aa6fe34a1e0c2f38c71d73fe4da95
John Doe <kubanam1995@gmail.com>
2020-09-13 17:33:31 +0200

    nj-timepicker was deleted

--- nj-timepicker-master/src/background/background.js
+++
@@ -1,8 +0,0 @@
-export class Background {
-
-    constructor() {
-        const element = document.createElement('div');
-        element.className = 'nj-overlay';
-        return element; // return the element to parent
-    }
-}
